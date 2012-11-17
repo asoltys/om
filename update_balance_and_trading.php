4e76f10e3f8c60f83f6967343f702ecacb75aa95
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2012-11-16 17:34:48 -0800

    Existence check

--- update_balance_and_trading.php
+++ update_balance_and_trading.php
@@ -7,37 +7,38 @@
 
   $db->real_query($query);
   $result = $db->store_result();
-  while($row = $result->fetch_assoc()) {
-    $query = "
-    SELECT trading_account, currency 
-    FROM om_repo WHERE id = " . $row['id'] . " 
-    INTO @trading_account, @currency;
-    
-    SET @balance = (SELECT COALESCE((
-      SELECT balance 
-      FROM om_repo 
-      WHERE trading_account = @trading_account 
-      AND currency = @currency
-      AND id <  " . $row['id'] . "
-      ORDER BY id DESC LIMIT 1)
-    ,0));
+  if ($result) {
+    while($row = $result->fetch_assoc()) {
+      $query = "
+      SELECT trading_account, currency 
+      FROM om_repo WHERE id = " . $row['id'] . " 
+      INTO @trading_account, @currency;
+      
+      SET @balance = (SELECT COALESCE((
+        SELECT balance 
+        FROM om_repo 
+        WHERE trading_account = @trading_account 
+        AND currency = @currency
+        AND id <  " . $row['id'] . "
+        ORDER BY id DESC LIMIT 1)
+      ,0));
 
-    SET @trading = (SELECT COALESCE((
-      SELECT trading 
-      FROM om_repo 
-      WHERE trading_account = @trading_account 
-      AND currency = @currency 
-      AND id <  " . $row['id'] . "
-      ORDER BY id DESC LIMIT 1)
-    ,0));
+      SET @trading = (SELECT COALESCE((
+        SELECT trading 
+        FROM om_repo 
+        WHERE trading_account = @trading_account 
+        AND currency = @currency 
+        AND id <  " . $row['id'] . "
+        ORDER BY id DESC LIMIT 1)
+      ,0));
 
-    UPDATE om_repo SET 
-      balance = @balance, 
-      trading = @trading
-    WHERE id = " . $row['id'] . ";";
-    echo "Updated record #" . $row['id'] . "<br />";
+      UPDATE om_repo SET 
+        balance = @balance, 
+        trading = @trading
+      WHERE id = " . $row['id'] . ";";
+      echo "Updated record #" . $row['id'] . "<br />";
 
-    $db->multi_query($query);
-    
+      $db->multi_query($query);
+    }
   }
 ?>
