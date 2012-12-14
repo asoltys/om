12e874eec7f1b48a97a623efe54916411ec541b2
Adam Soltys <adam.soltys@pwgsc-tpsgc.gc.ca>
2012-12-13 23:18:25 -0800

    Batch script using AJAX

---
+++ batch.php
@@ -0,0 +1,41 @@
+<? require('connect.php') ?>
+<?
+  if ($_GET['all']) {
+    $query = "SELECT id FROM om_repo";
+  } else {
+    $query = "
+      SELECT id FROM om_repo WHERE id > (SELECT max(id)
+      FROM om_repo 
+      WHERE flags = 'm')";
+  }
+
+  $db->real_query($query);
+  $result = $db->store_result();
+  if ($result) {
+    $ids = array();
+    while($row = $result->fetch_assoc()) {
+      $ids[] = $row['id'];
+    }
+    $ids = json_encode($ids);
+  }
+?>
+<? require('header.php') ?>
+<script type="text/javascript">
+  var ids = <? echo $ids; ?>;
+  var i = 0;
+
+  $(function() {
+    go();
+  });
+
+  function go() {
+    $.get('update_balance_and_trading.php', { id: ids[i] }, function() {
+      i++;
+      $('body').append(ids[i] + "<br>");
+      if (i < ids.length)
+        go();
+    });
+  }
+    
+</script>
+<? require('footer.php') ?>
--- update_balance_and_trading.php
+++ update_balance_and_trading.php
@@ -1,54 +1,37 @@
 <? require('connect.php') ?>
 <?
-  if ($_GET['all']) {
-    $query = "SELECT id FROM om_repo";
-  } else {
-    $query = "
-      SELECT id FROM om_repo WHERE id > (SELECT max(id)
-      FROM om_repo 
-      WHERE flags = 'm')";
-  }
+  $query = "
+  SELECT trading_account, currency, amount, transaction_id 
+  FROM om_repo WHERE id = " . $_GET['id'] . " 
+  INTO @trading_account, @currency, @amount, @transaction_id;
 
-  $db->real_query($query);
-  $result = $db->store_result();
-  if ($result) {
-    $query = "";
-    while($row = $result->fetch_assoc()) {
-      $query .= "
-      SELECT trading_account, currency, amount, transaction_id 
-      FROM om_repo WHERE id = " . $row['id'] . " 
-      INTO @trading_account, @currency, @amount, @transaction_id;
+  
+  SET @balance = (SELECT COALESCE((
+    SELECT balance 
+    FROM om_repo 
+    WHERE trading_account = @trading_account 
+    AND currency = @currency
+    AND id <  " . $_GET['id'] . "
+    ORDER BY id DESC LIMIT 1)
+  ,0)) + @amount;
 
-      
-      SET @balance = (SELECT COALESCE((
-        SELECT balance 
-        FROM om_repo 
-        WHERE trading_account = @trading_account 
-        AND currency = @currency
-        AND id <  " . $row['id'] . "
-        ORDER BY id DESC LIMIT 1)
-      ,0)) + @amount;
+  SET @trading = (SELECT COALESCE((
+    SELECT trading 
+    FROM om_repo 
+    WHERE trading_account = @trading_account 
+    AND currency = @currency 
+    AND id <  " . $_GET['id'] . "
+    ORDER BY id DESC LIMIT 1)
+  ,0)) + 
+  (SELECT IF(@transaction_id LIKE '%-r', 
+    -abs(@amount), 
+    abs(@amount))
+  );
 
-      SET @trading = (SELECT COALESCE((
-        SELECT trading 
-        FROM om_repo 
-        WHERE trading_account = @trading_account 
-        AND currency = @currency 
-        AND id <  " . $row['id'] . "
-        ORDER BY id DESC LIMIT 1)
-      ,0)) + 
-      (SELECT IF(@transaction_id LIKE '%-r', 
-        -abs(@amount), 
-        abs(@amount))
-      );
-
-      UPDATE om_repo SET 
-        balance = @balance, 
-        trading = @trading,
-        flags = 'm'
-      WHERE id = " . $row['id'] . ";";
-      $db->multi_query($query);
-    }
-    echo $query;
-  }
+  UPDATE om_repo SET 
+    balance = @balance, 
+    trading = @trading,
+    flags = 'm'
+  WHERE id = " . $_GET['id'] . ";";
+  $db->multi_query($query);
 ?>
